0430b5211f09211519de7112536a85590cc6fbb4
Robert Stauss <robertrstauss@gmail.com>
2020-02-13 13:28:17 -0500

    comments to block in code structure

--- index.js
+++ index.js
@@ -14,5 +14,28 @@ async function start() {
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
   // Now try and complete the program.
+
+  // guess a random number, and ask if it's correct
+  let guess = 50
+  let response = 'yes'
+
+  // if the player says "yes" exit the game
+  //if (response === 'yes') {
+  //  process.exit()
+  //}
+  // if the player says no
+  while (true) {
+    response = await ask('is it ' + guess)
+    //ask if it's higher or lower
+    // if it's higher modify min value
+    // if it's lower modify max value
+    //guess a new number
+    if(response === 'yes') {
+      break
+      //take some action
+    }
+    console.log('down here response is: ' + response)
+  }
+  console.log('some victory message')
   process.exit();
 }
